c474e195990a84f4f67c15acd2663ea6d19bef1d
Maxx-N <maximenorval@gmail.com>
2019-11-05 18:36:31 +0100

    tentative avec gmail

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,8 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
+=begin
+
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_LOGIN'],
   :password => ENV['SENDGRID_PWD'],
@@ -13,8 +15,8 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain,
   :enable_starttls_auto => true
 }
+=end
 
-=begin
 ActionMailer::Base.smtp_settings =   {
     :address            => 'smtp.gmail.com',
     :port               => 587,
@@ -23,4 +25,3 @@ ActionMailer::Base.smtp_settings =   {
     :user_name          => ENV['GMAIL_LOGIN'],
     :password           => ENV['GMAIL_PWD']
   }
-=end
